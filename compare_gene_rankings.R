9f92252dbe6fa6306de5647fd634ee0ecd6a27ca
Tiffany Ho <tiaho@ucdavis.edu>
2014-06-06 19:25:09 -0700

    added comments

--- compare_gene_rankings.R
+++ compare_gene_rankings.R
@@ -4,7 +4,7 @@
 # ranks the genes based on several features, ranks by total number of z scores away from mean
 # ranks based on cds length, exon length, intron length, codon usage
 #
-# need to avg the z scores for exons and introns
+# DON'T USE THIS
 
 setwd("~/Desktop/Lab/DataFiles/")
 
@@ -13,25 +13,33 @@ cds_length <- read.table("cds_lengths.txt", sep = ",")
 exon_length <- read.table("exon_lengths.txt", sep = ",")
 intron_length <- read.table("intron_lengths.txt", sep = ",")
 codon_usage <- read.table("gene_codon_usage_relative_entropies.txt", sep = ",")
+donor <- read.table("splice_site_donor_usage.txt", sep = ",")
+acceptor <- read.table("splice_site_acceptor_usage.txt", sep = ",")
+intron_usage <- read.table("intron_usage.txt", sep = ",")
 
-# puts the data frames of the parameters into an array
-parameters <- c(cds_length, exon_length, intron_length, codon_usage)
-
-# renames the columns
-colnames(cds_length) <- c('length', 'chromosome', 'gene')
-colnames(exon_length) <- c('length', 'chromosome', 'gene')
-colnames(intron_length) <- c('length', 'chromosome', 'gene')
-colnames(codon_usage) <- c('length', 'chromosome', 'gene')
+# log transformation
+cds_length[[4]] <- log(cds_length[[1]])
+exon_length[[4]] <- log(exon_length[[1]])
+intron_length[[4]] <- log(intron_length[[1]])
+codon_usage[[4]] <- log(codon_usage[[1]])
+donor[[4]] <- log(donor[[1]])
+acceptor[[4]] <- log(acceptor[[1]])
+intron_usage[[4]] <- log(intron_usage[[1]])
 
 # calculates z score
-cds_length$zscore <- (cds_length$length - mean(cds_length$length)) / sd(cds_length$length)
-cds_length[[4]] <- (cds_length[[1]] - mean(cds_length[[1]])) / sd(cds_length[[1]])
+cds_length[[5]] <- (cds_length[[4]] - mean(cds_length[[4]])) / sd(cds_length[[4]])
+exon_length[[5]] <- (exon_length[[4]] - mean(exon_length[[4]])) / sd(exon_length[[4]])
+intron_length[[5]] <- (intron_length[[4]] - mean(intron_length[[4]])) / sd(intron_length[[4]])
+codon_usage[[5]] <- (codon_usage[[4]] - mean(codon_usage[[4]])) / sd(codon_usage[[4]])
+donor[[5]] <- (donor[[4]] - mean(donor[[4]])) / sd(donor[[4]])
+acceptor[[5]] <- (acceptor[[4]] - mean(acceptor[[4]])) / sd(acceptor[[4]])
+intron_usage[[5]] <- (intron_usage[[4]] - mean(intron_usage[[4]])) / sd(intron_usage[[4]])
 
+cds_length$V2 <- as.character(cds_length$V2)
+  
+# exports file
+write.table(cds_length, "log_cds_lengths.csv", row.names = FALSE, col.names = TRUE, sep = ",")
 
+# merges the files into one
 
-# summaries
-summary(cds_length)
-summary(exon_length)
-summary(intron_length)
-summary(codon_usage)
-
+parameters <- c(cds_length, exon_length, intron_length, codon_usage, donor, acceptor, intron_usage)
--- genes_zscore_rank_distribution.R
+++ genes_zscore_rank_distribution.R
@@ -3,7 +3,7 @@
 
 setwd("~/Desktop/Lab/DataFiles/")
 
-zscores = read.table("zscore_rankings.txt", sep = ",")
+zscores = read.table("log_zscore_rankings.txt", sep = ",")
 colnames(zscores) = c("zscore", "chromosome", "gene")
 
 summary(zscores$zscore)
@@ -16,6 +16,24 @@ densityplot(zscores$zscore,
             main = list('Density Plot of Zscores of Gene Rankings', cex = 2),
             xlab= list('Zscores', cex = 1.5),
             ylab= list(cex = 1.5),
+            scales = list(cex = 1.2),
+            key = list(text = list(c('Mean', 'Upper Bound'), cex = 1.5),
+                       lines = list(type = 'l', lty = 1, col = c('green', 'red')),
+                       border = TRUE,
+                       columns = 2),
+            panel = function(...){
+              panel.densityplot(...)
+              panel.abline(v = upper_bound, col = 'red')
+              panel.abline(v = mean(zscores$zscore), col = 'green')
+            }
+)
+
+# zoomed in
+densityplot(zscores$zscore,
+            main = list('Density Plot of Zscores of Gene Rankings', cex = 2),
+            xlab= list('Zscores', cex = 1.5),
+            ylab= list(cex = 1.5),
+            xlim = c(0, 2),
             scales = list(cex = 1.2),
             key = list(text = list(c('Mean', 'Upper Bound'), cex = 1.5),
                        lines = list(type = 'l', lty = 1, col = c('green', 'red')),
