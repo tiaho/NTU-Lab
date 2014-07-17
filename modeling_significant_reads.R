6253016a1b902f64db749bb5c87de618d272b52e
Tiffany Ho <tiaho@ucdavis.edu>
2014-07-17 03:06:57 -0700

    now it actually does what it's supposed to

--- modeling_significant_reads.R
+++ modeling_significant_reads.R
@@ -3,51 +3,50 @@
 # 07/09/2014
 
 # models the NGS reads to determine what amount of reads is "significant"
+  
+# changes the working directory
+setwd("~/Desktop/NTU Lab/")
 
 # reads in data
-ngs <- read.table("~/Desktop/NTU Lab/NGS data.csv", sep = ",", header = T)
+ngs <- read.table("NGS data.csv", sep = ",", header = T)
 
 # need to remove kingdom, genus, species reads
 data <- subset(ngs, Level == "Phylum" | Level == "Class" | Level == "Order" | Level == "Family")
 
 # makes a vector with the sample names
 sample.names <- unique(data$SampleName)
-# test.names <- sample.names[1:2]
 
 # makes a vector with the level names
 data.levels <- unique(data$Level)
-# test.levels <- data.levels[1:2]
 
 # makes a vector with the % reads
-percent.of.reads <- seq(0.01, 0.50, by = 0.005)
-# test.percent <- seq(0.49, 0.50, by = 0.005)
+percent.of.reads <- seq(0.10, 0.90, by = 0.10)
 
 # summary vectors
 summary.sample <- vector()
 summary.level <- vector()
 summary.percent.reads <- vector()
+summary.number.of.reads <- vector()
 summary.stdev <- vector()
+
+# summary data frame
+summary.df <- data.frame()
 
 # 4 graphs on a page
 par(mfrow = c(2,2))
 
 
 # runs the simulation for each sample
-# for (i in "ZT2-C2"){
 for (i in sample.names){
   sample <- subset(data, SampleName == i)  # gets the sample
-#   print(sum(sample$Reads))
 
   # makes 2 vectors to keep track of % reads and % of level represented
   reads.data <- vector()
   level.data <- vector()
 
   # runs the simulation for each of the levels (phylum, class, order, family)
-#   for (j in "Phylum"){
   for (j in data.levels){
     sample.level <- subset(sample, Level == j) # only wants the data from individual levels
-#     print(sum(sample.level$Reads))
-#     print(sample.level.sorted <- sample.level[order(sample.level$Group),])
 
     # make a new data frame to sample from
     sample.level.data <- vector()
@@ -60,24 +59,28 @@ for (i in sample.names){
     }
 
     # runs the simulation for each of the % read values
-#     for (m in c(0.50, 0.10)){
     for (m in percent.of.reads){
       if ((as.integer(m * length(sample.level.data))) < 1) {next} # skips this % of reads sample if it results in no samples
-      samples <- sample(sample.level.data, as.integer(m * length(sample.level.data)), replace = FALSE) # samples from the data frame
-#       print(paste(i, j, m * 100, sep = " - "))
-      tmptable <- table(samples)/length(samples) * 100 # creates a table of % of each group represented
-#       print(tmptable)
+      
+      tmptable <- vector()
+      # runs the simulation 100 times
+      for (n in 1:100){
+        samples <- sample(sample.level.data, as.integer(m * length(sample.level.data)), replace = FALSE) # samples from the data frame
+        tmptable <- c(tmptable, as.vector(table(samples)/length(samples) * 100))
+  
+        # fills in 2 vectors with the % read values and % of each group represented
+        reads.data <- c(reads.data, rep(as.integer(m * length(sample.level.data)), length(tmptable)))
+        level.data <- c(level.data, tmptable)
 
-      # fills in 2 vectors with the % read values and % of each group represented
-      for (n in 1:length(tmptable)){
-        reads.data <- c(reads.data, m * length(sample.level.data))
-        level.data <- c(level.data, tmptable[n])
+        # prints the progress of the script
+        print(paste(i, j, m, n))
       }
 
       # data frame with: sample, level, % read, standard deviation
       summary.sample <- c(summary.sample, i)
       summary.level <- c(summary.level, j)
       summary.percent.reads <- c(summary.percent.reads, m)
+      summary.number.of.reads <- c(summary.number.of.reads, as.integer(m * length(sample.level.data)))
       summary.stdev <- c(summary.stdev, sd(tmptable))
     }
 
@@ -88,7 +91,7 @@ for (i in sample.names){
 }
 
 # makes a data frame of the summary
-summary.df <- data.frame(summary.sample, summary.level, summary.percent.reads, summary.stdev)
+summary.df <- data.frame(summary.sample, summary.level, summary.percent.reads, summary.number.of.reads, summary.stdev)
 
 # writes out the table to a csv file
-# write.table(summary.df, file = "~/Desktop/NTU Lab/stdev.csv", sep = ",", row.names = F)
+write.table(summary.df, file = "stdev.csv", sep = ",", row.names = F)
